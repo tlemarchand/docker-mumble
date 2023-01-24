668d9c82ac76225ff5647034d4530ed6e4b50762
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-01-24 07:10:47 +0100

    Update version

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 git checkout master
 podman run debian:bullseye-slim /bin/bash -c "apt-get update > /dev/null && apt-cache policy mumble-server | sed -n -e 's/^.*Candidate: //p' | tr -d '\n'" > version
 git add -A
-git commit -m `cat version`
+git commit -m "Update version"
 git push origin master
 git tag -f `cat version`
 git push origin -f `cat version`

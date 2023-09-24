43384f1652266712a1b553754085bcb4ce8eafbb
Thomas Lemarchand <tlemarchand@users.noreply.github.com>
2023-09-24 15:56:17 +0200

    Version update

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git checkout master
-podman run debian:bullseye-slim /bin/bash -c "apt-get update > /dev/null && apt-cache policy mumble-server | sed -n -e 's/^.*Candidate: //p' | tr -d '\n'" > version
+podman run debian:bookworm-slim /bin/bash -c "apt-get update > /dev/null && apt-cache policy mumble-server | sed -n -e 's/^.*Candidate: //p' | tr -d '\n'" > version
 git add -A
 git commit -m "Version update"
 git push origin master
